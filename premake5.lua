6e548373164b5b131ef4b58fd1abf1e586c76931
mdodis <michaeldodisgr@gmail.com>
2021-12-26 09:23:42 +0200

    add premake script

---
+++ premake5.lua
@@ -0,0 +1,21 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    staticruntime "off"
+    targetdir(BinDir)
+    objdir(ObjDir)
+
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
